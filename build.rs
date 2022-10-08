a84c6b3cd85c42684a6ed28d72ce57b13cca8630
Ingvar Stepanyan <me@rreverser.com>
2022-10-08 22:50:25 +0100

    pwsh

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("cargo:rerun-if-changed=generator/index.ts");
     println!("cargo:rerun-if-changed=generator/extra-schemas.ts");
     println!("cargo:rerun-if-changed=generator/server.ejs");
-    assert!(std::process::Command::new("cmd")
+    assert!(std::process::Command::new("pwsh")
         .arg("/c")
         .current_dir("generator")
         .arg("ts-node-esm")
