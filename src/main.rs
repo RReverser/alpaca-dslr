a28f27d0a6bcb2267703911502f873cc09a81bc6
Ingvar Stepanyan <me@rreverser.com>
2022-10-20 02:39:47 +0100

    Switch to axum for server-side

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{App, HttpServer};
 use ascom_alpaca_rs::api::{Camera, Device};
-use ascom_alpaca_rs::{ASCOMResult, DevicesBuilder, OpaqueResponse};
+use ascom_alpaca_rs::{ASCOMParams, ASCOMResult, DevicesBuilder, OpaqueResponse};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::prelude::*;
 
@@ -15,7 +16,7 @@ impl Device for MyCamera {
         &mut self,
         is_mut: bool,
         action: &str,
-        params: &str,
+        params: ASCOMParams,
     ) -> ASCOMResult<OpaqueResponse> {
         Camera::handle_action_impl(self, is_mut, action, params)
     }
@@ -23,21 +24,27 @@ impl Device for MyCamera {
 
 impl Camera for MyCamera {}
 
-#[actix_web::main]
-pub async fn main() -> std::io::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // initialize tracing
     tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_span_events(FmtSpan::CLOSE)
         .finish()
         .init();
 
     let devices = DevicesBuilder::new().with(MyCamera).finish();
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(tracing_actix_web::TracingLogger::default())
-            .service(devices.clone())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    // run our app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(
+            devices
+                .into_router()
+                .layer(TraceLayer::new_for_http())
+                .into_make_service(),
+        )
+        .await
+        .unwrap();
 }
