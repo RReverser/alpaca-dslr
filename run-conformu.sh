1d033f98e45ff2f79dac1b86d9bb410426122ac4
Ingvar Stepanyan <me@rreverser.com>
2023-04-04 12:55:28 +0100

    Fix log path config for ascom-alpaca

--- run-conformu.sh
+++ run-conformu.sh
@@ -1,5 +1,5 @@
 cargo build
-RUST_LOG=ascom_alpaca_rs=debug,alpaca_dslr=debug,gphoto2=debug,gphoto2::gp_port_vusb_find_device_lib=warn cargo run &
+RUST_LOG=ascom_alpaca=debug,alpaca_dslr=debug,gphoto2=debug,gphoto2::gp_port_vusb_find_device_lib=warn cargo run &
 RUST=$!
 ~/conformu/conformu --commandline --settings $PWD/conform.settings.json
 kill $RUST
